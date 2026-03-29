8bb002891a59e5a95201006547cf8edc03f77441
edugrowth5-rgb <edugrowth5@gmail.com>
2026-03-29 16:12:53 +0530

    Create STsw.js

---
+++ STsw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'storyteller-v1';
+
+// In files ko browser apne paas save kar lega (Offline ke liye)
+const ASSETS = [
+  './STindex.html',
+  './STstyle.css',
+  './STapp.js',
+  './STstories.js',
+  './STmanifest.json',
+  'https://cdn-icons-png.flaticon.com/512/1067/1067256.png'
+];
+
+// 1. Install Event: Files ko cache mein save karna
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('ST Cache Opened');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Activate Event: Purane cache ko delete karna (jab aap app update karein)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('ST Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Fetch Event: Internet na hone par Cache se file uthana
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Agar cache mein file mil gayi toh wahi return karo, nahi toh network se lo
+      return response || fetch(event.request);
+    })
+  );
+});
